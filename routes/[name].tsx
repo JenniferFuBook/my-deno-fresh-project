c0d2db4ef2a2fd29f74b7074673fb28d6835e9b3
Jennifer Fu <jennifer.fu@dominodatalab.com>
2022-07-24 11:02:51 -0700

    initial commit

---
+++ routes/[name].tsx
@@ -0,0 +1,25 @@
+/** @jsx h */
+import { Fragment, h } from "preact";
+import { Handlers, PageProps } from "$fresh/server.ts";
+
+export const handler: Handlers<string | null> = {
+  async GET(_, ctx) {
+    const resp = await fetch('http://localhost:8000/api/joke');
+    console.log(resp);
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const result = await resp.text();
+    return ctx.render(result);
+  },
+};
+
+export default function Greet(props: PageProps) {
+  console.log(props);
+  return (
+    <Fragment>
+      <div>Hello {props.params.name}</div>
+      <div>Joke for you: {props.data}</div>
+    </Fragment>
+  );
+}
